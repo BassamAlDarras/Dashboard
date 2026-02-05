90d782f4fecd9d30e70fb6517541f25b9e852e2d
Bassam AlDarras <bassam.ib.darras@gmail.com>
2026-02-05 13:33:57 +0400

    sync

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dd6aead1f8c3e809"; }
+/******/ 		__webpack_require__.h = function() { return "7473eb6f391aada5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
